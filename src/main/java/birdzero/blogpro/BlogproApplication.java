60f840f93947b0174554faaf1e41b243c289e82c
birdzero <redviking2@naver.com>
2023-03-21 06:41:35 +0900

    initial commit

---
+++ src/main/java/birdzero/blogpro/BlogproApplication.java
@@ -0,0 +1,13 @@
+package birdzero.blogpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/birdzero/blogpro/BlogproApplicationTests.java
@@ -0,0 +1,13 @@
+package birdzero.blogpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
