0bfb8ed142178150f4858a4a837fc55aa4e0120e
birdzero <redviking2@naver.com>
2023-03-21 20:15:10 +0900

    feat: Reply 엔티티 작성

--- src/main/java/birdzero/blogpro/model/Reply.java
+++ src/main/java/birdzero/blogpro/model/Reply.java
@@ -6,40 +6,32 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
-import java.sql.Timestamp;
-import java.util.List;
 
+import java.sql.Timestamp;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Entity
-public class User {
+public class Reply {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
-    @Column(nullable = false, length = 50)
-    private String username;
-
-    @Column(nullable = false, length = 100)
-    private String password;
-
-    @Column(nullable = false, length = 50)
-    private String email;
-
+    @Column(nullable = false,length = 100)
+    private String content;
 
     @CreationTimestamp
     private Timestamp createDate;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "userId")
+    @JoinColumn(name = "boarId")
+    private Board board;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="userId")
     private User user;
-
-    @OneToMany(mappedBy = "board")
-    private List<Reply> replies;
-
-
 }
