f4bee64406e9c4e3cd93898959d0e281df924e29
birdzero <redviking2@naver.com>
2023-03-23 00:13:49 +0900

    fix: navi fragment 이름 수정

--- src/main/java/birdzero/blogpro/BlogproApplication.java
+++ src/main/java/birdzero/blogpro/BlogproApplication.java
@@ -2,8 +2,11 @@ package birdzero.blogpro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class BlogproApplication {
 
 	public static void main(String[] args) {
