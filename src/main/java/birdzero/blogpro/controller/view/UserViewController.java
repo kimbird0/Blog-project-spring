939c865fee774f9a3b4f24a18c3a5d1d1433392f
birdzero <redviking2@naver.com>
2023-03-23 00:37:11 +0900

    refactor: form, layout 구분

--- src/main/java/birdzero/blogpro/controller/view/UserViewController.java
+++ src/main/java/birdzero/blogpro/controller/view/UserViewController.java
@@ -1,14 +1,8 @@
 package birdzero.blogpro.controller.view;
 
-import birdzero.blogpro.dto.ResponseDto;
-import birdzero.blogpro.model.User;
-import birdzero.blogpro.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 //view 컨트롤러와 api 컨트롤러를 분리해야함
 //책임분리및 가독성, 테스트 용이
