85aa110d8433e629a633c8929d3eae799af53b3c
birdzero <redviking2@naver.com>
2023-04-14 23:52:15 +0900

    comment: board(게시물) 컨트롤러 관련 주석 작성

--- src/main/java/birdzero/blogpro/controller/api/BoardApiController.java
+++ src/main/java/birdzero/blogpro/controller/api/BoardApiController.java
@@ -15,21 +15,24 @@ import org.springframework.web.bind.annotation.*;
 public class BoardApiController {
     private final BoardService boardService;
 
+    //게시물 작성
     @PostMapping("/api/board")
     public ResponseDto<Integer> save(@RequestBody SaveBoardDto saveBoardDto, @AuthenticationPrincipal PrincipalDetail principal){
-        boardService.register(saveBoardDto.toEntity(), principal.getUser().getId());
-        return new ResponseDto<Integer>(HttpStatus.OK.value(),1);
+        //데이터 형식(이 경우 JSON)을 애플리케이션에서 사용할 수 있는 개체로 변환(deserialize)
+        boardService.register(saveBoardDto.toEntity(), principal.getUser().getId()); //새 보드 저장
+        return new ResponseDto<Integer>(HttpStatus.OK.value(),1);       //HTTP 상태 코드 200과 데이터 값 1 반환
     }
-    @DeleteMapping("/api/board/{id}")
+    //게시물 삭제
+    @DeleteMapping("/api/board/{id}")                                               //id는 경로 변수로 제공
     public ResponseDto<Integer> deleteById(@PathVariable int id){
-        boardService.delete(id);
-        return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+        boardService.delete(id);                                                    //보드 삭제
+        return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);      //HTTP 상태 코드 200과 데이터 값 1 반환
     }
-
-    @PutMapping("/api/board/{id}")
+    //게시물 수정
+    @PutMapping("/api/board/{id}")                                                  //id는 경로 변수로 제공
     public ResponseDto<Integer> update(@PathVariable int id, @RequestBody Board board){
         boardService.updateBoard(id, board);
-        return new ResponseDto<Integer>(HttpStatus.OK.value(),1);
+        return new ResponseDto<Integer>(HttpStatus.OK.value(),1);       //HTTP 상태 코드 200과 데이터 값 1 반환
     }
 
 }
